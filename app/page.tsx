7880b530599214505a0c90fa7d06c1fea99e4ce3
suu-b <thakur0805@gmail.com>
2025-05-14 20:09:08 +0530

    refactor: added shadcn lib

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,26 @@ import { signIn } from "next-auth/react"
 import { motion } from 'framer-motion'
 import { useState } from "react"
 import { MoonLoader } from "react-spinners"
+import { Button } from "@/components/ui/button"
 
+/**
+ * The component for the home page.
+ * It displays a banner image, a heading, and a button to sign in with Google.
+ */
 const Home: React.FC = () => {
+  /**
+   * Loading state to show the loading spinner when the user clicks the sign in button. Solely to cover the time taken by the next-auth to redirect to the user-auth page/the google sign in card.
+   */
   const [loading, setIsLoading] = useState<Boolean>(false)
+
+  /**
+   * The function sets the loading to true and calls the inbuilt signIn function from next-auth to sign in with Google. Next-auth will handle the rest of the authentication process. And Once the user is authenticated, it will redirect to the user-auth page.
+   */
   const handleGoogleSignIn = () => {
     setIsLoading(true)
     signIn("google", { callbackUrl: "/user-auth" })
   }
+
   return (
     <main>
       <motion.div
@@ -34,7 +47,11 @@ const Home: React.FC = () => {
           <h1 className="font-black text-slate-900 text-7xl">HARMONIA</h1>
           <p className="text-slate-600 text-base w-[85%] text-center mt-1">Organize, sync and access your notes effortlessly with our web-app, seamlessly integrated with Google Drive for ultimate convenience</p>
           <h2 className="text-lg text-slate-700 font-extrabold my-3">Research. Create. Note. Sync</h2>
-          <div className="h-24">{loading ? <MoonLoader className="my-2" size={25} /> : <button id="continue-with-google" onClick={handleGoogleSignIn} className="bg-blue-400 hover:bg-blue-500 px-5 py-2 rounded text-white flex justify-between items-center">Continue with Google<FaGoogle size={18} className="ml-1" /></button>
+          <div className="h-24">{loading ? <MoonLoader className="my-2" size={25} /> : 
+              <Button onClick={handleGoogleSignIn} variant="default" size="lg" className="gap-2">
+                Continue with Google
+                <FaGoogle size={18} />
+              </Button>
           }</div>
         </section>
       </motion.div>
@@ -43,3 +60,4 @@ const Home: React.FC = () => {
 }
 
 export default Home
+
--- app/user-auth/page.tsx
+++ app/user-auth/page.tsx
@@ -1,67 +1,101 @@
 "use client"
 
-import { BeatLoader } from 'react-spinners'
+import { useEffect } from 'react'
 import Image from "next/image"
 import { useSession } from "next-auth/react"
-import { motion } from "framer-motion"
-import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import { BeatLoader } from 'react-spinners'
+import { motion } from "framer-motion"
+import {
+  Card,
+  CardContent,
+  CardTitle,
+  CardHeader,
+  CardDescription
+} from '@/components/ui/card'
+
 import setCookie from '@/utils/setCookie'
 
-
+/**
+ * * UserAuthPage component
+ * * This component is used to display the user authentication page.
+ * * It shows a welcome message and a loading spinner while the user is being authenticated.
+ */
 const UserAuthPage: React.FC = () => {
-    const { data: session, status } = useSession()
-    const router = useRouter()
+  const { data: session, status } = useSession()
+  const router = useRouter()
 
+    /**
+     * Once the user is authenticated and the access Token is generated, the access token is set in the cookie and the user is redirected to the workspace page with an intentional delay of 3 seconds.
+     * The delay is used to show the loading spinner and the welcome message to the user.
+     */
+  useEffect(() => {
+    if (session?.user) {
+      if (session?.accessToken) {
+        setCookie({
+          cookieName: "accessToken",
+          cookieVal: session.accessToken,
+          expiryDays: 0.04,
+        })
+      }
 
-    useEffect(() => {
-        if (session?.user) {
-            if (session?.accessToken) {
-                setCookie({ cookieName: "accessToken", cookieVal: session?.accessToken, expiryDays: 0.04 })
-            }
-            setTimeout(() => {
-                router.push('workspace')
-            }, 3000)
-        }
-    }, [session?.user])
-
-
-    if (status === "loading") {
-        return <p>Loading...</p>
+      setTimeout(() => {
+        router.push('workspace')
+      }, 3000)
     }
-    if (!session) {
-        return <p>You are not signed in</p>
-    }
+  }, [session?.user])
 
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-                delay: 0.3,
-                duration: 0.8,
-                ease: "easeInOut",
-            }}
-        >
-            <div className="grid-background h-screen w-screen bg-gray-100 flex flex-col justify-center items-center">
-                <div className="bg-white w-fit h-fit shadow border rounded p-10 flex flex-col justify-center items-center">
-                    <h3 className="text-lg text-slate-800 tracking-wider">HARMONIA</h3>
-                    <h1 className="text-4xl font-bold text-slate-800 my-3">Welcome, <span className="text-blue-300">{session.user?.name}</span></h1>
-                    {session.user?.image && (
-                        <Image
-                            src={session.user.image}
-                            alt="Profile Image"
-                            width={100}
-                            height={100}
-                            className="rounded-full"
-                        />
-                    )}
-                    <p className='font-semibold text-slate-600 mt-5 mb-2'>Harmonia is setting up your workspace...</p>
-                    <BeatLoader color="#1E293B" size={15} />
-                </div>
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+    >
+      <div className="grid-background bg-gray-100 flex flex-col justify-center items-center min-h-screen p-6">
+        {status === "authenticated" ? (
+          <Card className='w-fulltext-center p-10'>
+            <CardContent className='flex flex-col justify-center items-center'>
+              <CardHeader className="text-lg tracking-wider text-slate-800">HARMONIA</CardHeader>
+              <CardTitle className='text-4xl font-bold my-3'>
+                Welcome, <span className="text-blue-300">{session.user?.name}</span>
+              </CardTitle>
+              {session.user?.image && (
+                <Image
+                  src={session.user.image}
+                  alt="Profile Image"
+                  width={150}
+                  height={150}
+                  className="rounded-full"
+                />
+              )}
+              <p className='font-semibold text-slate-600 mt-5 mb-2'>
+                Harmonia is setting up your workspace...
+              </p>
+              <BeatLoader color="#1E293B" size={15} />
+            </CardContent>
+          </Card>
+        ) : status === "loading" ? (
+          <Card className='w-full max-w-md text-center p-10'>
+            <CardContent className='flex flex-col justify-center items-center'>
+              <CardDescription className='text-xl font-bold my-3'>Loading...</CardDescription>
+              <BeatLoader color="#1E293B" size={15} />
+            </CardContent>
+          </Card>
+        ) : (
+          <Card className='w-full max-w-md text-center p-10'>
+            <CardContent className='flex flex-col justify-center items-center'>
+              <CardDescription >We don't think you've signed in yet.</CardDescription>
+              <CardDescription>Please Log in and try again.</CardDescription>
+            </CardContent>
+          </Card>
+        )}
+      </div>
+    </motion.div>
+  )
 }
 
 export default UserAuthPage
