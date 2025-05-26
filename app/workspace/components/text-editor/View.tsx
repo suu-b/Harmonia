e91a99ba056d5b71f630cea8638cbcb917e4e2dc
suu-b <thakur0805@gmail.com>
2025-05-26 19:32:13 +0530

    feat: text-editor is functional

--- app/workspace/components/text-editor/View.tsx
+++ app/workspace/components/text-editor/View.tsx
@@ -1,8 +1,24 @@
+"use client";
+
 import React from "react";
 import ToolBar from "./ToolBar";
 import Worksheet from "./Worksheet";
+import { useMemo } from "react";
+import { createMyEditor } from "harmonia-text-editor";
 
+/**
+ * View Component for the Text Editor
+ * It contains the toolbar and the worksheet.
+ */
 const View: React.FC = () => {
+  const editor = useMemo(() => createMyEditor(), []);
+  const initialValue = [
+    {
+      type: "paragraph",
+      children: [{ text: "Write from here..." }],
+    },
+  ];
+
   return (
     <div className="flex flex-col w-full h-full p-4 justify-start items-center">
       <p className="text-sm text-slate-600 text-left w-full my-3">
@@ -10,10 +26,10 @@ const View: React.FC = () => {
         <span className="text-slate-900">Breadcrumb</span>
       </p>
       <section className="sticky top-0 z-50 shadow">
-        <ToolBar />
+        <ToolBar editor={editor} />
       </section>
       <hr className="my-5" />
-      <Worksheet />
+      <Worksheet editor={editor} initialValue={initialValue} />
     </div>
   );
 };
